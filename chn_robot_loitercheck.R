00e389b5420d21ed56b689d66fcce098ee09146e
B Steele <steeleb@caryinstitute.org>
2022-01-24 09:05:34 -0700

    make loiter check runs for lakes

---
+++ chn_robot_loitercheck.R
@@ -0,0 +1,78 @@
+# Auburn robot quick vis to check loiters
+
+library(tidyverse)
+library(sf)
+library(tmap)
+
+# determine OS for automatic filepath determination
+os <- osVersion
+path_pat = NULL
+
+if (grepl('win', os, ignore.case = T) == T ){
+  path_pat = 'Z:/'
+  message('Windows OS detected')
+} else if (grepl('mac', os, ignore.case = T) == T ){
+  path_pat = '/Volumes/EpscorBlooms/'
+  message('Mac OS detected')
+} else {
+  message('OS path pattern not detected. Please store OS path pattern manually.')
+}
+
+# point to directories
+data_dir = paste0(path_pat, 'project_data/ASV_data/analysis_ready/CHN/')
+layers_dir = paste0(path_pat, 'lake_spatial_data/NHD_shapefiles/')
+map_dir = paste0(path_pat, 'project_data/ASV_data/intermediary/map_reality_check/')
+
+# Load shapefiles of lakes ----
+
+#get the layers inside the gdb
+st_layers(file.path(layers_dir, 'NHDLakeShapefiles.gdb'))
+
+#read in the GDB
+ME <- st_read('C:/Users/steeleb/Dropbox/EPSCoR_shapefiles/NHDLakeShapefiles.gdb', 'me_lakes')
+
+#subset for CHN
+CHN <- ME[ME$GNIS_Name == 'China Lake',]
+# make into a simple feature
+CHN <- st_as_sf(CHN)
+#get the crs
+st_crs(CHN)
+
+#reporject into WGS84, UTM 19N
+CHN_reproj <- st_transform(CHN, crs = 'EPSG:32619')
+#check projection
+st_crs(CHN_reproj)
+
+# run loop over data to do first pass of processing, looking at loiter flag ----
+
+#get list of robot runs
+robo_runs <- list.files(data_dir)
+
+for(i in 1:length(robo_runs)) {
+  #read in robot data
+  CHN_robot_data <- read.csv(file.path(data_dir, robo_runs[i]))
+  #make into simple feature; crs is WGS84
+  CHN_robot_georef <- st_as_sf(CHN_robot_data, coords = c('longitude_gps_deg', 'latitude_gps_deg'), crs = 'EPSG:4326')
+  #reproject to WGS UTM19N
+  CHN_robot_georef_reproj <- st_transform(CHN_robot_georef, 'EPSG:32619')
+  #check projection
+  st_crs(CHN_robot_georef_reproj)
+
+  #get the extent of the robot run for focus
+  bbox_robot = st_bbox(CHN_robot_georef_reproj)
+
+  #make CHN basemeap from robot extent
+  CHN_map = tm_shape(CHN_reproj, bbox = bbox_robot) +
+    tm_borders()
+
+  #add a layer to check projection and loiter locs
+  CHN_map_robot = CHN_map +
+    tm_shape(CHN_robot_georef_reproj) +
+    tm_dots(col = 'loiter_flag',
+            palette = c('black', 'orange')) +
+    tm_layout(inner.margins = c(0.2, 0.1, 0.2, 0.1),
+      title = paste0(robo_runs[i], ' quick vis'),
+      legend.position = c('left', 'bottom'))
+  filename = paste0(CHN_robot_data$lake[1], '_', CHN_robot_data$date[1], '_', CHN_robot_data$deployment_instance[1], '.png')
+  tmap_save(CHN_map_robot, file.path(map_dir, filename))
+}
